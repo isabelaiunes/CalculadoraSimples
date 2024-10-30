4eb10cb6de00a6eb80f22c9f8cf53dfc1099400f
Isabela Iunes <iunesisabela.cs@gmail.com>
2024-10-30 17:35:29 -0300

    master

--- CalculadoraSimples.py
+++ CalculadoraSimples.py
@@ -1,18 +1,22 @@
-print ("Bem-vindo a Calculadora!")
-num1 = float(input( "Insira o primeiro número: " ))
-num2 = float(input( "Insira o segundo número "))
-operação = input( "Selecione uma operação (+, -, *, /): ")
-if operação == "+":
+print("\n******************* Calculadora em Python *******************")
+print("Selecione o número da operação desejada")
+print("1 - Soma, 2- Subtração, 3- Multiplicação, 4- Divisão")
+operacao = (input('Qual operação você quer fazer? '))
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+if operacao == "1":
     resultado = num1 + num2
     print("O resultado é: ", resultado)
-elif operação == "-":
+elif operacao == "2":
     resultado = num1 - num2
     print("O resultado é: ", resultado)
-elif operação == "*":
+elif operacao == "3":
     resultado = num1 * num2
     print("O resultado é: ", resultado)
-elif operação == "/":
-    resultado = num1 / num2
-    print("O resultado é: ", resultado)
+elif operacao == "4":
+    if num2 == 0:  # Verifica se o divisor é zero
+        print("Número não divisível por zero.")
+    else:
+        resultado = num1 % num2
 else:
-    print("Operação Inválida.")
+    print('Operação Inválida. Selecione um dos números acima.')
